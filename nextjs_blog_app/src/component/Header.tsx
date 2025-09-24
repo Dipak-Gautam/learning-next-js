062bbb6e07b11cd8bbade108e2ca73be61e7fce0
Dipak-Gautam <anjangautam095@gmail.com>
2025-09-24 14:55:06 +0545

    learning the routing in next js

---
+++ nextjs_blog_app/src/component/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-amber-500 p-3 text-white font-medium text-xl flex items-center justify-center gap-5 ">
+      <Link href={"/"} className="hover:text-slate-300 ">
+        Home
+      </Link>
+      <Link href={"/posts"} className="hover:text-slate-300 ">
+        {" "}
+        Posts
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
